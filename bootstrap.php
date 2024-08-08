e3020fc1b1a47276248e0ca944105d32680fafb9
amir <amirjoun2@gmail.com>
2024-08-08 20:45:42 +0400

    Added config/path_constants.php for constants directories

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,7 @@ use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 
 require __DIR__ ."/vendor/autoload.php";
+require __DIR__ ."/configs/path_constants.php";
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
---
+++ configs/path_constants.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+const APP_PATH = dirname(__DIR__) . '/app';
+const VIEW_PATH = dirname(__DIR__) . '/resources/views';
+const STORAGE_PATH = dirname(__DIR__) . '/storage';
+const CONFIG_PATH = dirname(__DIR__) . '/configs';
+const BUILD_PATH = dirname(__DIR__) . '/public/build';
