fbf815f04d8b28966e5d02712612a69a1fff1e81
amir <amirjoun2@gmail.com>
2024-08-13 20:41:38 +0400

    Installed some symfony packages, some doctrine packages, and twig/intl-extra, Added AppEnvironment.php Enum for defining some App configs environment, Added some classes to container_bindings.php for auto bindings

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -3,8 +3,56 @@
 declare(strict_types=1);
 
 use App\Config;
+use Slim\Views\Twig;
 use function DI\create;
+use Doctrine\ORM\ORMSetup;
+use App\Enum\AppEnvironment;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
+use Twig\Extra\Intl\IntlExtension;
+use Symfony\Component\Asset\Package;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Asset\Packages;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
+
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
 
 return [
     Config::class => create(Config::class)->constructor(require CONFIG_PATH . "/app.php"),
+    Twig::class => function (Config $config, ContainerInterface $container) {
+        $twig = Twig::create(VIEW_PATH, [
+            'cache' => STORAGE_PATH . '/cache/templates',
+            'auto_reload' => AppEnvironment::isDevelopment($config->get('app_environment')),
+        ]);
+
+        $twig->addExtension(new IntlExtension());
+        $twig->addExtension(new EntryFilesTwigExtension($container));
+        $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
+        
+        return $twig;
+    },
+    EntityManager::class =>  function(Config $config) {
+        $configure = ORMSetup::createAttributeMetadataConfiguration(
+            paths: $config->get('doctrine.entity_dir'),
+            isDevMode: $config->get('doctrine.dev_mode'),
+        );
+        
+        return new EntityManager(
+        DriverManager::getConnection($config->get('doctrine.connection'), $configure),
+        $configure,
+        );
+    },
+    'webpack_encore.packages' => fn() => new Packages(
+        new Package(new JsonManifestVersionStrategy(
+            BUILD_PATH . '/manifest.json'
+    ))),
+    'webpack_encore.entrypoint_lookup' => fn() => new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
+    'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
+        new EntrypointLookupCollection($container->get('webpack_encore.entrypoint_lookup')),
+        $container->get('webpack_encore.packages')
+    ),
 ];
