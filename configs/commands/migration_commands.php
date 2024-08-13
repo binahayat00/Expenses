bdcc3a530f71597332520071a41a11967f5701d6
amir <amirjoun2@gmail.com>
2024-08-13 20:51:03 +0400

    Added commands directory in configs for running some commands

---
+++ configs/commands/migration_commands.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Tools\Console\Command;
+
+return fn(DependencyFactory $dependencyFactory) => [
+    new Command\CurrentCommand($dependencyFactory),
+    new Command\DumpSchemaCommand($dependencyFactory),
+    new Command\ExecuteCommand($dependencyFactory),
+    new Command\GenerateCommand($dependencyFactory),
+    new Command\LatestCommand($dependencyFactory),
+    new Command\MigrateCommand($dependencyFactory),
+    new Command\RollupCommand($dependencyFactory),
+    new Command\StatusCommand($dependencyFactory),
+    new Command\VersionCommand($dependencyFactory),
+    new Command\UpToDateCommand($dependencyFactory),
+    new Command\SyncMetadataCommand($dependencyFactory),
+    new Command\ListCommand($dependencyFactory),
+    new Command\DiffCommand($dependencyFactory),
+];
