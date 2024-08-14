273e4a554c103075637447a6fdb24d86b437087a
amir <amirjoun2@gmail.com>
2024-08-15 00:34:32 +0400

    Added HomeController, middleware.php,migrations.php, routes/web.php

---
+++ configs/middleware.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Config;
+use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+return function (App $app) {
+    $container = $app->getContainer();
+    $config = $container->get(Config::class);
+
+    $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+    $app->addErrorMiddleware(
+        (bool) $config->get('desplay_error_details'),
+        (bool) $config->get('log_errors'),
+        (bool) $config->get('log_error_details'),
+    );
+};
