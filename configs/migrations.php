255c4c2c58d0508ad73f590b5b377a4d245b45a8
amir <amirjoun2@gmail.com>
2024-08-17 19:32:30 +0400

    Renamed APP_ENVIRONMENT in .env, Added expenses file for commands, Reformed migrations.php config base in the new version

--- configs/migrations.php
+++ configs/migrations.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 return [
     'table_storage' => [
-        'table_name' => 'migrations',
+        'table_name' => 'doctrine_migration_versions',
         'version_column_name' => 'version',
-        'version_column_length' => 1024,
+        'version_column_length' => 191,
         'executed_at_column_name' => 'executed_at',
         'execution_time_column_name' => 'execution_time',
     ],
@@ -16,7 +16,7 @@ return [
     'all_or_nothing' => true,
     'transactional' => true,
     'check_database_platform' => true,
-    'optimize_migrations' => 'none',
+    'organize_migrations' => 'none',
     'connection' => null,
     'em' => null,
 ];
