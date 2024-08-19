7021808ccf49f1a7f2708493aca9d385f53427ac
amir <amirjoun2@gmail.com>
2024-08-19 22:49:00 +0400

    Added relationships and the Getter & Setter to Entities, and Migrated Entities to database for creating new tables

--- app/Entity/Category.php
+++ app/Entity/Category.php
@@ -6,7 +6,11 @@ use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[Entity, Table('categories')]
 class Category
@@ -22,4 +26,82 @@ class Category
 
     #[Column(name: 'updated_at')]
     private \DateTime $updatedAt;
+
+    #[OneToMany(mappedBy: 'category', targetEntity: Transaction::class)]
+    private Collection $transactions;
+    
+    #[ManyToOne(inversedBy: 'categories')]
+    private User $user;
+
+    public function __construct()
+    {
+        $this->transactions = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): Category
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): Category
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): Category
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
+
+    public function addTransaction(Transaction $transaction): Category
+    {
+        $this->transactions->add($transaction);
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): Category
+    {
+        $user->addCategory($this);
+
+        $this->user = $user;
+
+        return $this;
+    }
 }
--- app/Entity/Receipt.php
+++ app/Entity/Receipt.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
@@ -22,4 +23,62 @@ class Receipt
 
     #[Column(name: 'updated_at')]
     private \DateTime $updatedAt;
+
+    #[ManyToOne(inversedBy: 'receipts')]
+    private Transaction $transaction;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName(string $fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+ 
+    public function getTransaction(): Transaction
+    {
+        return $this->transaction;
+    }
+
+    public function setTransaction(Transaction $transaction)
+    {
+        $transaction->addReceipt($this);
+
+        $this->transaction = $transaction;
+
+        return $this;
+    }
 }
