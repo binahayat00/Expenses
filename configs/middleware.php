786d85d73c463f4d83d0e56cad01f3073e2e39bd
amir <amirjoun2@gmail.com>
2024-08-22 22:09:52 +0400

    Using StartSessionsMiddleware

--- configs/middleware.php
+++ configs/middleware.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Config;
+use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
 use Slim\Views\Twig;
@@ -14,8 +15,8 @@ return function (App $app) {
     $config = $container->get(Config::class);
 
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
-
     $app->add(ValidationExceptionMiddleware::class);
+    $app->add(StartSessionsMiddleware::class);
 
     $app->addErrorMiddleware(
         (bool) $config->get('display_error_details'),
