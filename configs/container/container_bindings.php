1c97fc8e68dcc88221ab79ed81ebff260d3f8d3e
amir <amirjoun2@gmail.com>
2024-08-28 23:48:17 +0400

    Added csrf to the container_bindings.php

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -6,6 +6,7 @@ use App\Auth;
 use Slim\App;
 use App\Config;
 use App\Session;
+use Slim\Csrf\Guard;
 use Slim\Views\Twig;
 use App\Enum\SameSite;
 use function DI\create;
@@ -96,4 +97,7 @@ return [
     )
     ),
     RequestValidatorFactoryInterface::class => fn(ContainerInterface $container) => $container->get(RequestValidatorFactory::class),
+    'csrf', fn(ResponseFactoryInterface $responseFactory) => new Guard(
+        $responseFactory,
+        persistentTokenMode: true),
 ];
