d804fee51d067137a93ed956ed3b1475ce84d348
amir <amirjoun2@gmail.com>
2024-10-02 23:46:41 +0400

    Added the store method for TransactionController, Added TransactionData, TransactionRequestValidator, TransactionService

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -3,12 +3,17 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
+use DateTime;
 use Slim\Views\Twig;
 use App\ResponseFormatter;
 use App\Services\RequestService;
 use App\Services\CategoryService;
+use App\DataObjects\TransactionData;
+use App\Services\TransactionService;
 use Psr\Http\Message\RequestInterface as Request;
+use App\RequestValidators\RequestValidatorFactory;
 use Psr\Http\Message\ResponseInterface as Response;
+use App\RequestValidators\TransactionRequestValidator;
 
 class TransactionController
 {
@@ -16,7 +21,9 @@ class TransactionController
         private readonly Twig $twig, 
         private readonly ResponseFormatter $responseFormatter,
         private readonly RequestService $requestService,
-        private readonly CategoryService $categoryService
+        private readonly CategoryService $categoryService,
+        private readonly TransactionService $transactionService,
+        private readonly RequestValidatorFactory $requestValidatorFactory,
     )
     {
     }
@@ -28,6 +35,25 @@ class TransactionController
             'transactions/index.twig',
             ['categories' => $this->categoryService->getCategoryName()] 
         );
+    }
+
+    public function store(Request $request, Response $response): Response
+    {
+        $data = $this->requestValidatorFactory->make(TransactionRequestValidator::class)->validate(
+            $request->getParsedBody()
+        );
+
+        $this->transactionService->create(
+            new TransactionData(
+                $data['description'],
+                (float) $data['amount'],
+                new DateTime($data['date']),
+                $data['category']
+            ),
+            $request->getAttribute('user')
+        );
+
+        return $response;
     }
 
     
