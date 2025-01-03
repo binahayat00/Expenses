29cfb98635209ed124674d04a661f747115f8d24
amir <amirjoun2@gmail.com>
2025-01-03 23:58:32 +0400

    Implemented SignedUrl class

--- app/SignedUrl.php
+++ app/SignedUrl.php
@@ -2,7 +2,31 @@
 
 namespace App;
 
+use Slim\Interfaces\RouteParserInterface;
+
 class SignedUrl
 {
-    
+    public function __construct(private readonly Config $config, private readonly RouteParserInterface $routeParser)
+    {
+
+    }
+    public function fromRoute(
+        string $routeName, 
+        array $routeParams, 
+        \DateTime $expirationDate
+        ): string
+    {
+        $expiration = $expirationDate->getTimestamp();
+        $queryParams = ['expiration' => $expiration ];
+        $baseUrl = trim($this->config->get('app_url'), '/');
+        $url = $baseUrl . $this->routeParser->urlFor($routeName, $routeParams, $queryParams);
+
+        $signature = hash_hmac('sha256', $url, $this->config->get('app_key'));
+        // {BASE_URL}/verify/{USER_ID}/{EMAIL_HASH}?expiration={EXPIRATION_TIMESTAMP}&signature={SIGNATURE}
+        return $baseUrl . $this->routeParser->urlFor(
+            $routeName,
+            $routeParams,
+            $queryParams + ['signature' => $signature]
+        );
+    }
 }
