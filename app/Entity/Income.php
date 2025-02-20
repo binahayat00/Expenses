2fcdeed43639daeb5447f1640e61505d08007a66
amir <amirjoun2@gmail.com>
2025-02-20 23:45:02 +0400

    Added getter and setter methods

--- app/Entity/Income.php
+++ app/Entity/Income.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Entity;
+use DateTime;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
@@ -27,15 +29,61 @@ class Income
     private string $source;
 
     #[Column]
-    private \DateTime $date;
-
-    #[Column(name: 'created_at')]
-    private \DateTime $createdAt;
-
-    #[Column(name: 'updated_at')]
-    private \DateTime $updatedAt;
+    private DateTime $date;
 
     #[ManyToOne(inversedBy: 'incomes')]
     private User $user;
     
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount($amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getSource(): string
+    {
+        return $this->source;
+    }
+
+    public function setSource($source): self
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate($date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser($user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
 }
